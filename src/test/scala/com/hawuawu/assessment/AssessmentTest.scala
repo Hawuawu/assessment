9d212988ef060ee3d406c5d6a9dd680e748a43a9
dannovak <dan.novak@creativedock.cz>
2018-11-15 22:52:51 +0100

    Minor indents refactoring and removing println in test.

--- src/test/scala/com/hawuawu/assessment/AssessmentTest.scala
+++ src/test/scala/com/hawuawu/assessment/AssessmentTest.scala
@@ -4,28 +4,27 @@ import com.hawuawu.assessment.util.{File, Grouping}
 import org.scalatest.FlatSpec
 
 class AssessmentTest extends FlatSpec with Grouping with File {
- "Assessment" should "create groups in proper way" in {
+  "Assessment" should "create groups in proper way" in {
 
     val fileName = "example.csv"
     val occupancyData = loadAddressesFromResource(fileName)
     val groups = createGroups(occupancyData)
 
-   println(groups)
     assert(groups.size == 3)
 
-   assert(
-     groups
-       .exists(g => g.fromDate == 5 & g.toDate == 12 & g.addressId == "ADR001" && g.numberOfCustomers == 3)
-   )
+    assert(
+      groups
+        .exists(g => g.fromDate == 5 & g.toDate == 12 & g.addressId == "ADR001" && g.numberOfCustomers == 3)
+    )
 
-   assert(
-     groups
-       .exists(g => g.fromDate == 1 & g.toDate == 5 & g.addressId == "ADR001" & g.numberOfCustomers == 2)
-   )
+    assert(
+      groups
+        .exists(g => g.fromDate == 1 & g.toDate == 5 & g.addressId == "ADR001" & g.numberOfCustomers == 2)
+    )
 
-   assert(
-     groups
-       .exists(g => g.fromDate == 7 & g.toDate == 11 & g.addressId == "ADR003" & g.numberOfCustomers == 2)
-   )
+    assert(
+      groups
+        .exists(g => g.fromDate == 7 & g.toDate == 11 & g.addressId == "ADR003" & g.numberOfCustomers == 2)
+    )
   }
 }
